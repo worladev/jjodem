82e0da593ee370c328a861c25d47630075dfe365
worladev <confidenceworlasi@gmail.com>
2024-03-06 11:35:17 +0000

    added model for rental details

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CarBrand, BrandModel
+from .models import CarBrand, BrandModel, RentalDetails
 
 # Register your models here.
 admin.site.register(CarBrand)
 admin.site.register(BrandModel)
+admin.site.register(RentalDetails)
