9d18afe31b0023cb9762dc5b832cd6b5814ae715
worladev <confidenceworlasi@gmail.com>
2024-03-06 00:18:06 +0000

    media files configuration

--- jjodemrentals/urls.py
+++ jjodemrentals/urls.py
@@ -22,9 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rentals.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # SERVING STATIC FILES LIKE THIS IS NOT EFFICIENT FOR PRODUCTION
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
