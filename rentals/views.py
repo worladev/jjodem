449869a3f59d6028e5c2812b2a8e9cb49a5d9845
worladev <confidenceworlasi@gmail.com>
2024-03-19 04:23:58 +0000

    building the home template to display car types

--- rentals/views.py
+++ rentals/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import CarType
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'rentals/home.html')
+    car_type = CarType.objects.all()
+    context = {'car_type':car_type}
+    return render(request, 'rentals/home.html', context)
+
+
     # checkout django template inheritance
