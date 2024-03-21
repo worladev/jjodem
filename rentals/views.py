72c4ecb681e18dcc65e2716ab7933608cf3c41cb
worladev <confidenceworlasi@gmail.com>
2024-03-21 23:10:23 +0000

    modifying view and url for the home template

--- rentals/views.py
+++ rentals/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import CarType
+from .models import CarType, ShopInfo
 
 
 # Create your views here.
 def home(request):
     car_type = CarType.objects.all()
-    context = {'car_type':car_type}
+    shop_info = ShopInfo.objects.all()
+    context = {
+        'car_type': car_type,
+        'shop_info': shop_info,
+        }
     return render(request, 'rentals/home.html', context)
-
-
-    # checkout django template inheritance
+# checkout django template inheritance
