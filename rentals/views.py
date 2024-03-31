9703cd83431839c2413c962a131e03045bd5338b
worladev <confidenceworlasi@gmail.com>
2024-03-31 19:08:51 +0000

    used decorator to implement shop info

--- rentals/views.py
+++ rentals/views.py
@@ -1,21 +1,38 @@
+from functools import wraps
 from django.shortcuts import render
 from .models import CarType, ShopInfo, CarModel
 
 
 # Create your views here.
-def home(request):
+def add_shop_info(view_info_func):
+    @wraps(view_info_func)
+    def _wrapped_view(request, *args, **kwargs):
+        shop_info = ShopInfo.objects.all()
+        response = view_info_func(request, *args, shop_info=shop_info, **kwargs)
+        if isinstance(response, dict):
+            response['shop_info'] = shop_info
+        return response
+    return _wrapped_view
+
+
+@add_shop_info
+def home(request, shop_info):
     car_type = CarType.objects.all()
-    shop_info = ShopInfo.objects.all()
     context = {
         'car_type': car_type,
         'shop_info': shop_info,
         }
     return render(request, 'rentals/home.html', context)
-# checkout django template inheritance
 
 
-def filter(request, type_name):
-    filter_car = CarModel.objects.filter(type="")
-    context = {'car': filter_car}
-    return render(request, 'rentals/filter.html', context)
+@add_shop_info
+def filter_cars(request, car_type_id, shop_info):
+    car_type = CarType.objects.get(pk=car_type_id)
+    filter_car = CarModel.objects.filter(type=car_type)
+    context = {
+        'car_type': car_type,
+        'filter_car': filter_car,
+        'shop_info': shop_info,
+        }
+    return render(request, 'rentals/filter-cars.html', context)
     
