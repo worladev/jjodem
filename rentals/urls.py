bd9590398be3e74da9fa6c01b7463f3a5892daf4
worladev <confidenceworlasi@gmail.com>
2024-04-02 19:02:47 +0000

    added view for car detail and inventory list

--- rentals/urls.py
+++ rentals/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('type/<int:car_type_id>/', views.filter_cars, name="filter_cars")
+    path('type/<int:car_type_id>/', views.filter_cars, name="filter_cars"),
+    path('inventory/', views.inventory_list, name='inventory-list'),
+    path('detail/<int:car_id>/', views.car_detail, name='car-detail'),
 ]
--- rentals/views.py
+++ rentals/views.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from django.shortcuts import render
+from django.http import Http404
 from .models import CarType, ShopInfo, CarModel
 
 
@@ -28,11 +29,33 @@ def home(request, shop_info):
 @add_shop_info
 def filter_cars(request, car_type_id, shop_info):
     car_type = CarType.objects.get(pk=car_type_id)
-    filter_car = CarModel.objects.filter(type=car_type)
+    filter_car = CarModel.objects.filter(type=car_type, is_available=True)
     context = {
         'car_type': car_type,
         'filter_car': filter_car,
         'shop_info': shop_info,
         }
     return render(request, 'rentals/filter-cars.html', context)
-    
+
+
+@add_shop_info
+def inventory_list(request, shop_info):
+    car_models = CarModel.objects.filter(is_available=True)
+    context = {
+        'car_models': car_models,
+        'shop_info': shop_info,
+    }
+    return render(request, 'rentals/inventory-list.html', context)
+
+
+@add_shop_info
+def car_detail(request, car_id, shop_info):
+    try:
+        car = CarModel.objects.get(pk=car_id)
+    except CarModel.DoesNotExist:
+        raise Http404('Car not available')
+    context = {
+        'car': car,
+        'shop_info': shop_info,
+    }
+    return render(request, 'rentals/car-detail.html', context)
