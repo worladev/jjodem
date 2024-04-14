da6265dca3f374f010e8521d816d5da7d05a54b0
worladev <confidenceworlasi@gmail.com>
2024-04-14 08:49:28 +0000

    built the search view

--- rentals/views.py
+++ rentals/views.py
@@ -1,7 +1,9 @@
 from functools import wraps
 from django.shortcuts import render, Http404, get_object_or_404
+from django.http import HttpResponse
 from .models import CarType, ShopInfo, CarModel
 from .recommender import get_similar_cars
+from django.db.models import Q
 
 
 # Create your views here.
@@ -52,10 +54,10 @@ def inventory_list(request, shop_info):
 def car_detail(request, car_id, shop_info):
     try:
         car = get_object_or_404(CarModel, pk=car_id)
-        similar_cars = get_similar_cars(car.type)
+        similar_cars = get_similar_cars(car.type) # recommendation function call
     except CarModel.DoesNotExist:
         raise Http404('Car not available')
-    
+    # exclude the current car from the recommended cars
     similar_cars = [similar_car for similar_car in similar_cars if similar_car.id != car.id]
     context = {
         'car': car,
@@ -71,3 +73,31 @@ def about(request, shop_info):
         'shop_info': shop_info
     }
     return render(request, 'rentals/about.html', context)
+
+
+@add_shop_info
+def search_view(request, shop_info):
+    if request.method == 'GET':
+        query = request.GET.get('search-data')
+        if query:
+            # sanitize query to prevent SQL injection attacks
+            safe_query = query.strip()  # remove leading and trailing whitespaces
+            if safe_query:
+                # case-insensitive search
+                search_car = CarModel.objects.filter(
+                    Q(model_name__icontains=safe_query) |
+                    Q(description__icontains=safe_query)
+                )
+            else:
+                search_car = CarModel.objects.none()  # empty query
+        else:
+            search_car = CarModel.objects.all()  # empty query
+    else:
+        return HttpResponse("Method Not Allowed", status=405)  # non-GET request
+    
+    context = {
+        'shop_info': shop_info,
+        'search_car': search_car,
+    }
+    return render(request, 'rentals/search.html', context)
+        
