3495aeb1a3088ab79d437e75bafdb87310db7a41
worladev <confidenceworlasi@gmail.com>
2024-04-15 11:04:54 +0000

    modified decorator for info and handles

--- rentals/views.py
+++ rentals/views.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from django.shortcuts import render, Http404, get_object_or_404
 from django.http import HttpResponse  #
-from .models import CarType, ShopInfo, CarModel
+from .models import CarType, ShopInfo, CarModel, SocialHandle
 from .recommender import get_similar_cars
 from django.db.models import Q  #
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
@@ -12,47 +12,52 @@ def add_shop_info(view_info_func):
     @wraps(view_info_func)
     def _wrapped_view(request, *args, **kwargs):
         shop_info = ShopInfo.objects.all()
-        response = view_info_func(request, *args, shop_info=shop_info, **kwargs)
+        social_handle = SocialHandle.objects.all()
+        response = view_info_func(request, *args, shop_info=shop_info, social_handle=social_handle, **kwargs)
         if isinstance(response, dict):
             response['shop_info'] = shop_info
+            response['social_info'] = social_handle
         return response
     return _wrapped_view
 
 
 @add_shop_info
-def home(request, shop_info):
+def home(request, shop_info, social_handle):
     car_type = CarType.objects.filter(is_available=True)
     context = {
         'car_type': car_type,
         'shop_info': shop_info,
+        'social_handle': social_handle,
         }
     return render(request, 'rentals/home.html', context)
 
 
 @add_shop_info
-def filter_cars(request, car_type_id, shop_info):
+def filter_cars(request, car_type_id, shop_info, social_handle):
     car_type = CarType.objects.get(pk=car_type_id)
     filter_car = CarModel.objects.filter(type=car_type, is_available=True)
     context = {
         'car_type': car_type,
         'filter_car': filter_car,
         'shop_info': shop_info,
+        'social_handle': social_handle,
         }
     return render(request, 'rentals/filter-cars.html', context)
 
 
 @add_shop_info
-def inventory_list(request, shop_info):
+def inventory_list(request, shop_info, social_handle):
     car_models = CarModel.objects.filter(is_available=True)
     context = {
         'car_models': car_models,
         'shop_info': shop_info,
+        'social_handle': social_handle,
     }
     return render(request, 'rentals/inventory-list.html', context)
 
 
 @add_shop_info
-def car_detail(request, car_id, shop_info):
+def car_detail(request, car_id, shop_info, social_handle):
     try:
         car = get_object_or_404(CarModel, pk=car_id)
         similar_cars = get_similar_cars(car.type) # recommendation function call
@@ -64,20 +69,22 @@ def car_detail(request, car_id, shop_info):
         'car': car,
         'shop_info': shop_info,
         'similar_cars': similar_cars,
+        'social_handle': social_handle,
     }
     return render(request, 'rentals/car-detail.html', context)
 
 
 @add_shop_info
-def about(request, shop_info):
+def about(request, shop_info, social_handle):
     context = {
-        'shop_info': shop_info
+        'shop_info': shop_info,
+        'social_handle': social_handle,
     }
     return render(request, 'rentals/about.html', context)
 
 
 @add_shop_info
-def search_view(request, shop_info):
+def search_view(request, shop_info, social_handle):
     query = None
     safe_query = None
     results = []
@@ -106,6 +113,6 @@ def search_view(request, shop_info):
         'shop_info': shop_info,
         'safe_query': safe_query,
         'results': results,
+        'social_handle': social_handle,
     }
     return render(request, 'rentals/search.html', context)
-        
