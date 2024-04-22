5156f3d4ae37fc55ce3c266157bd464fdf55483f
worladev <confidenceworlasi@gmail.com>
2024-04-22 22:37:00 +0000

    debugged recommendation exclude error

--- rentals/recommender.py
+++ rentals/recommender.py
@@ -1,7 +1,7 @@
 from .models import CarModel
 
 
-def get_similar_cars(car_type, number_of_rec=5):
+def get_similar_cars(car_type, number_of_rec=7):
     cars_of_type = CarModel.objects.filter(type=car_type)
-    similar_cars = cars_of_type.exclude(id=car_type.pk)[:number_of_rec]
+    similar_cars = cars_of_type[:number_of_rec]
     return similar_cars
